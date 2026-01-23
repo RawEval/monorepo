6899249c8dd2b60af874a3049ab29e0872c4a994
Durgesh Kumar <code.durgesh@gmail.com>
2026-01-24 03:17:12 +0530

    fix: pixel code

--- apps/landing/app/layout.tsx
+++ apps/landing/app/layout.tsx
@@ -61,9 +61,9 @@ export default function RootLayout({
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
             n.queue=[];t=b.createElement(e);t.async=!0;
             t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '906259072138243');
+            fbq('init', '1860126784625257');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -73,6 +73,13 @@ export default function RootLayout({
             width="1"
             style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=906259072138243&ev=PageView&noscript=1"
+            alt=""
+          />
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1860126784625257&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
